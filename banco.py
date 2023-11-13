c883320a47c0915ab45a2d2139da04313d6895bb
eddieallen84 <eddieallen84@github.com>
2023-11-13 04:08:32 -0400

    finalizado

--- banco.py
+++ banco.py
@@ -27,8 +27,7 @@ while True:
             print("Operação falhou! O Valor informado é inválido")
 
     elif opcao == "s":
-        saque = float(input("Valor do saque: "))
-
+        valor = float(input("Valor do saque: "))
         excedeu_saldo = valor > saldo
         excedeu_limite = valor > limite
         excedeu_saques = numero_saques >= LIMITE_SAQUES
@@ -38,9 +37,18 @@ while True:
             print("Operação falhou! O valor excedeu o limite por saque")
         elif excedeu_saques:
             print("Operação falhou! Número máximo de saques excedido")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f'Saque: R${valor:.2f} '
+            numero_saques += 1
+        else:
+            print('Operação falhou! O valor informado é inválido.')
 
     elif opcao == "e":
-        print("Extrato")
+        print("\n================= Extrato =================")
+        print('Não foram realizadas movimentações' if not extrato else extrato)
+        print(f'\nSaldo: R$ {saldo:.2f} ')
+        print('===========================================')
 
     elif opcao == "q":
         break
