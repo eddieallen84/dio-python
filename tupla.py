c89717a83a3ff9ae7332ddd58fc99695db6155d1
eddieallen84 <eddieallen84@github.com>
2023-11-16 16:04:30 -0400

    exemplos de tupla

---
+++ tupla.py
@@ -0,0 +1,22 @@
+frutas = ("pera", "laranja", "abacaxi", "morango",)
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil",)
+
+matriz = (
+    (1, "a", 3),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[-1])
+print(frutas[-3])
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[0][-1])
+print(matriz[-1][-1])
